dbdfd5f257b2432126f6f4edffd12ee168f05405
Furqon Ramadhani <ahfura70@gmail.com>
2026-08-16 10:08:47 +0700

    Add Firebase services Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version "2.0.20" apply false
     id("com.google.devtools.ksp") version "2.3.2" apply false
     id("com.google.dagger.hilt.android") version "2.57" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
