0eb2391d3e8573bd8cf673cfd07d02620c41e1cc
Furqon Ramadhani <ahfura70@gmail.com>
2026-08-17 04:21:40 +0700

    Return inserted schedule id for reminder scheduling

--- app/src/main/java/com/mytask/data/local/dao/ScheduleDao.kt
+++ app/src/main/java/com/mytask/data/local/dao/ScheduleDao.kt
@@ -11,62 +11,29 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ScheduleDao {
 
-    @Query(
-        """
-        SELECT * FROM schedules
-        ORDER BY dayOfWeek ASC, startMinutes ASC
-        """
-    )
+    @Query("SELECT * FROM schedules ORDER BY dayOfWeek ASC, startMinutes ASC")
     fun getAllSchedules(): Flow<List<ScheduleEntity>>
 
-    @Query(
-        """
-        SELECT * FROM schedules
-        ORDER BY dayOfWeek ASC, startMinutes ASC
-        """
-    )
+    @Query("SELECT * FROM schedules ORDER BY dayOfWeek ASC, startMinutes ASC")
     suspend fun getAllSchedulesSnapshot(): List<ScheduleEntity>
 
-    @Query(
-        """
-        SELECT * FROM schedules
-        WHERE id = :id
-        """
-    )
-    fun getScheduleById(
-        id: Long
-    ): Flow<ScheduleEntity?>
+    @Query("SELECT * FROM schedules WHERE id = :id")
+    fun getScheduleById(id: Long): Flow<ScheduleEntity?>
 
-    @Query(
-        """
-        SELECT * FROM schedules
-        WHERE dayOfWeek = :dayOfWeek
-        ORDER BY startMinutes ASC
-        """
-    )
-    fun getSchedulesByDay(
-        dayOfWeek: Int
-    ): Flow<List<ScheduleEntity>>
+    @Query("SELECT * FROM schedules WHERE dayOfWeek = :dayOfWeek ORDER BY startMinutes ASC")
+    fun getSchedulesByDay(dayOfWeek: Int): Flow<List<ScheduleEntity>>
 
     @Insert
-    suspend fun insert(
-        schedule: ScheduleEntity
-    )
+    suspend fun insert(schedule: ScheduleEntity): Long
 
     @Insert
-    suspend fun insertAll(
-        schedules: List<ScheduleEntity>
-    )
+    suspend fun insertAll(schedules: List<ScheduleEntity>)
 
     @Update
-    suspend fun update(
-        schedule: ScheduleEntity
-    )
+    suspend fun update(schedule: ScheduleEntity)
 
     @Delete
-    suspend fun delete(
-        schedule: ScheduleEntity
-    )
+    suspend fun delete(schedule: ScheduleEntity)
 
     @Query("DELETE FROM schedules")
     suspend fun deleteAll()
