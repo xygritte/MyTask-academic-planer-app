70a083c545caca4c3df49f31015d405765f9d616
Furqon Ramadhani <184005838+xygritte@users.noreply.github.com>
2026-08-16 16:05:24 +0700

    Update MainActivity.kt

--- app/src/main/java/com/mytask/MainActivity.kt
+++ app/src/main/java/com/mytask/MainActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
