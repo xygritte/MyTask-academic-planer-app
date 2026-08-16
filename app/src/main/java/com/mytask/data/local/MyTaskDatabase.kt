31e2fe99faccaa8362b6183eccb2c0503a22048f
Furqon Ramadhani <ahfura70@gmail.com>
2026-08-16 20:55:04 +0700

    Bump Room schema for task completion timestamp

--- app/src/main/java/com/mytask/data/local/MyTaskDatabase.kt
+++ app/src/main/java/com/mytask/data/local/MyTaskDatabase.kt
@@ -16,7 +16,7 @@ import com.mytask.data.local.entity.TaskEntity
         CourseEntity::class,
         ScheduleEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
