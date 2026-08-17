97dab1dbed0d8dae0c8a1d349c718a56b2eb985f
Furqon Ramadhani <ahfura70@gmail.com>
2026-08-17 20:23:15 +0700

    feat(schedule): bump Room schema for multi-range schedules

--- app/src/main/java/com/mytask/data/local/MyTaskDatabase.kt
+++ app/src/main/java/com/mytask/data/local/MyTaskDatabase.kt
@@ -16,7 +16,7 @@ import com.mytask.data.local.entity.TaskEntity
         CourseEntity::class,
         ScheduleEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
